93a0fd675639e143bd5caed4abadd0e0d04070c7
salman261002 <salmanalfarisi261002@gmail.com>
2024-04-26 00:33:05 +0700

    add toggle and devMode logic

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ import DotPattern from "@/components/magicui/dot-pattern";
 import { Button, DarkMode } from "@/components/ui/button";
 
 // Datas
-import { Blogs, Links, Products, Socials } from "@/content";
+import { Blogs, Links, Products, Socials, Toggle } from "@/content";
 import { RectangleCard, SquareCard } from "@/components/ui/card";
 import { RxStar, RxStarFilled } from "react-icons/rx";
 
 export default function Home() {
   return (
-    <main className="flex h-full max-h-screen flex-col items-center justify-between sm:p-24 bg-accent animate overflow-hidden">
+    <main className="flex h-full grow flex-col items-center justify-between animate sm:py-24">
       <section
         id="home"
-        className="flex flex-col overflow-y-auto items-center w-full sm:max-w-sm h-full grow sm:border sm:rounded-2xl overflow-hidden px-6 py-6 gap-y-3 relative bg-background animate"
+        className="flex flex-col items-center w-full sm:max-w-sm h-full grow px-6 py-6 gap-y-3 relative bg-background animate"
       >
         {/* Magic UI */}
         <DotPattern
@@ -50,43 +50,54 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex w-full items-end justify-center gap-x-3 mb-4 flex-wrap z-10">
-          {Socials.map((social, index) => (
-            <Link key={index} href={social.href}>
-              <social.icon className="w-5 h-5 text-foreground animate" />
-            </Link>
-          ))}
-        </div>
+        {/* Socials */}
+        {Toggle.socials && (
+          <div className="flex w-full items-end justify-center gap-x-3 mb-4 flex-wrap z-10">
+            {Socials.map((social, index) => (
+              <Link key={index} href={social.href}>
+                <social.icon className="w-5 h-5 text-foreground animate" />
+              </Link>
+            ))}
+          </div>
+        )}
 
         {/* Bio */}
-        <div className="flex flex-col gap-y-2 h-fit w-full p-2 rounded-xl border bg-popover backdrop-blur-sm animate z-10">
-          <p className="text-clamp-sm text-popover-foreground w-fit animate">
-            Creative Fullstack Developer Focusing on Next.js, Tailwind CSS and
-            Supabase.
-          </p>
-          <button className="bg-primary text-primary-foreground border w-full py-2 rounded-lg hover:bg-primary/90 font-medium text-clamp-sm animate">
-            Let's Connect
-          </button>
-        </div>
+        {Toggle.socials && (
+          <div className="flex flex-col gap-y-2 h-fit w-full p-2 rounded-xl border bg-popover backdrop-blur-sm animate z-10">
+            <p className="text-clamp-sm text-popover-foreground w-fit animate">
+              Creative Fullstack Developer Focusing on Next.js, Tailwind CSS and
+              Supabase.
+            </p>
+            <button className="bg-primary text-primary-foreground border w-full py-2 rounded-lg hover:bg-primary/90 font-medium text-clamp-sm animate">
+              Let's Connect
+            </button>
+          </div>
+        )}
 
-        {/* Social Links */}
-        {Links.map((link, index) => (
-          <Button key={index} subtext={link.subtext} icon={<link.icon size={20}/>}>
-            {link.name}
-          </Button>
-        ))}
+        {/* Links */}
+        {Toggle.links &&
+          Links.map((link, index) => (
+            <Button
+              key={index}
+              subtext={link.subtext}
+              icon={<link.icon size={20} />}
+            >
+              {link.name}
+            </Button>
+          ))}
 
         {/* Product Square */}
-        <div className="grid grid-cols-2 w-full gap-3 z-10">
-          {Products.map((product, index) => (
-            <SquareCard key={index} product={product} />
-          ))}
-        </div>
+        {Toggle.products && (
+          <div className="grid grid-cols-2 w-full gap-3 z-10">
+            {Products.map((product, index) => (
+              <SquareCard key={index} product={product} />
+            ))}
+          </div>
+        )}
 
         {/* Blog Card */}
-        {Blogs.map((blog, index) => (
-          <RectangleCard key={index} blog={blog} />
-        ))}
+        {Toggle.blogs &&
+          Blogs.map((blog, index) => <RectangleCard key={index} blog={blog} />)}
       </section>
     </main>
   );
